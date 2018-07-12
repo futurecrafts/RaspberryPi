5469315e2108a3c5a84909a23f0a9dd9d8ba7628
futurecrafts <biny38317@naver.com>
2018-07-12 21:58:21 +0900

    Update playsound.py

--- HouseGuard/playsound.py
+++ HouseGuard/playsound.py
@@ -27,33 +27,33 @@ import pygame
 pygame.mixer.pre_init(44100, -16, 12, 512)
 pygame.init()
 
-kick = pygame.mixer.Sound('samples/kick.wav')
+kick = pygame.mixer.Sound('kick.wav')
 kick.set_volume(.65);
-snare = pygame.mixer.Sound('samples/snare.wav')
+snare = pygame.mixer.Sound('snare.wav')
 snare.set_volume(.65);
-openhh = pygame.mixer.Sound('samples/open.wav')
+openhh = pygame.mixer.Sound('open.wav')
 openhh.set_volume(.65);
-closedhh = pygame.mixer.Sound('samples/closed.wav')
+closedhh = pygame.mixer.Sound('closed.wav')
 closedhh.set_volume(.65);
-clap = pygame.mixer.Sound('samples/clap.wav')
+clap = pygame.mixer.Sound('clap.wav')
 clap.set_volume(.65);
-cymbal = pygame.mixer.Sound('samples/cymbal.wav')
+cymbal = pygame.mixer.Sound('cymbal.wav')
 cymbal.set_volume(.65);
 
 while True:
   
 	kick.play()
-  print( 'kick was just played')
+  	print( 'kick was just played')
 	snare.play()
-  print( 'snare was just played')
+  	print( 'snare was just played')
 	openhh.play()
-  print( 'openhh was just played')
+  	print( 'openhh was just played')
 	closedhh.play()
-  print( 'closedhh was just played')
+  	print( 'closedhh was just played')
 	clap.play()
-  print( 'clap was just played')
+  	print( 'clap was just played')
 	cymbal.play()
-  print( 'cymbal was just played')
+  	print( 'cymbal was just played')
   
 #  def distance():
     # set Trigger to HIGH
