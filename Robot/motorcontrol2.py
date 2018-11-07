215ff8bda02cb22d591ef3b04c1cfaade6917305
futurecrafts <biny38317@naver.com>
2018-11-07 20:44:11 +0900

    Update motorcontrol2.py

--- Robot/motorcontrol2.py
+++ Robot/motorcontrol2.py
@@ -1,5 +1,6 @@
 from time import sleep
 import RPi.GPIO as GPIO
+import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -9,49 +10,55 @@ def clean():
         GPIO.output(23, GPIO.LOW)
         GPIO.output(24, GPIO.LOW)
 
-def carmove(towhere):
+def carmove(towhere, wait):
         if towhere == 'go':
                         GPIO.output(17, GPIO.HIGH)
                         GPIO.output(22, GPIO.LOW)
                         GPIO.output(23, GPIO.HIGH)
                         GPIO.output(24, GPIO.LOW)
-						time.sleep(wait)
+                        time.sleep(float(wait))
+                        clean()
                         print "car Go"
         elif towhere == 'back':
                         GPIO.output(17, GPIO.LOW)
                         GPIO.output(22, GPIO.HIGH)
                         GPIO.output(23, GPIO.LOW)
                         GPIO.output(24, GPIO.HIGH)
-						time.sleep(wait)
+                        time.sleep(float(wait))
+                        clean()
                         print "car Back"
         elif towhere == 'left':
                         GPIO.output(17, GPIO.LOW)
                         GPIO.output(22, GPIO.LOW)
                         GPIO.output(23, GPIO.HIGH)
                         GPIO.output(24, GPIO.LOW)
-						time.sleep(wait)
+                        time.sleep(float(wait))
+                        clean()
                         print "car Left"
         elif towhere == 'right':
                         GPIO.output(17, GPIO.HIGH)
                         GPIO.output(22, GPIO.LOW)
                         GPIO.output(23, GPIO.LOW)
                         GPIO.output(24, GPIO.LOW)
-						time.sleep(wait)
+                        time.sleep(float(wait))
+                        clean()
                         print "car Right"
-		elif towhere == 'pleft':
+        elif towhere == 'pright':
                         GPIO.output(17, GPIO.HIGH)
                         GPIO.output(22, GPIO.LOW)
                         GPIO.output(23, GPIO.LOW)
                         GPIO.output(24, GPIO.HIGH)
-						time.sleep(wait)
-                        print "car pivot Left"
-        elif towhere == 'pright':
+                        time.sleep(float(wait))
+                        clean()
+                        print "car pivot Right"
+        elif towhere == 'pleft':
                         GPIO.output(17, GPIO.LOW)
                         GPIO.output(22, GPIO.HIGH)
                         GPIO.output(23, GPIO.HIGH)
                         GPIO.output(24, GPIO.LOW)
-						time.sleep(wait)
-                        print "car pivot Right"
+                        time.sleep(float(wait))
+                        clean()
+                        print "car pivot Left"
         elif towhere == 'stop':
                         clean()
                         print "car Stop"
@@ -59,7 +66,7 @@ def carmove(towhere):
 if __name__ == '__main__':
         import sys
         towhere = sys.argv[1]
-		wait = sys.argv[2]
+        wait = sys.argv[2]
         GPIO.setup(17, GPIO.OUT)
         GPIO.setup(22, GPIO.OUT)
         GPIO.setup(23, GPIO.OUT)
