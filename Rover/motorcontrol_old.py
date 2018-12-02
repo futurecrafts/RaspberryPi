06eb987b86bba5d5754d753d5c75c7119d742c35
futurecrafts <biny38317@naver.com>
2018-12-02 16:05:46 +0900

    Update motorcontrol_old.py

--- Rover/motorcontrol_old.py
+++ Rover/motorcontrol_old.py
@@ -4,35 +4,35 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 def clean():
-        GPIO.output(17, GPIO.LOW)
-        GPIO.output(22, GPIO.LOW)
-        GPIO.output(23, GPIO.LOW)
-        GPIO.output(24, GPIO.LOW)
+        GPIO.output(12, GPIO.LOW)
+        GPIO.output(13, GPIO.LOW)
+        GPIO.output(19, GPIO.LOW)
+        GPIO.output(26, GPIO.LOW)
 
 def carmove(towhere):
         if towhere == 'go':
-                        GPIO.output(17, GPIO.HIGH)
-                        GPIO.output(22, GPIO.LOW)
-                        GPIO.output(23, GPIO.HIGH)
-                        GPIO.output(24, GPIO.LOW)
+                        GPIO.output(12, GPIO.HIGH)
+                        GPIO.output(13, GPIO.LOW)
+                        GPIO.output(19, GPIO.HIGH)
+                        GPIO.output(26, GPIO.LOW)
                         print "Rover Go"
         elif towhere == 'back':
-                        GPIO.output(17, GPIO.LOW)
-                        GPIO.output(22, GPIO.HIGH)
-                        GPIO.output(23, GPIO.LOW)
-                        GPIO.output(24, GPIO.HIGH)
+                        GPIO.output(12, GPIO.LOW)
+                        GPIO.output(13, GPIO.HIGH)
+                        GPIO.output(19, GPIO.LOW)
+                        GPIO.output(26, GPIO.HIGH)
                         print "Rover Back"
         elif towhere == 'left':
-                        GPIO.output(17, GPIO.LOW)
-                        GPIO.output(22, GPIO.LOW)
-                        GPIO.output(23, GPIO.HIGH)
-                        GPIO.output(24, GPIO.LOW)
+                        GPIO.output(12, GPIO.LOW)
+                        GPIO.output(13, GPIO.LOW)
+                        GPIO.output(19, GPIO.HIGH)
+                        GPIO.output(26, GPIO.LOW)
                         print "Rover Left"
         elif towhere == 'right':
-                        GPIO.output(17, GPIO.HIGH)
-                        GPIO.output(22, GPIO.LOW)
-                        GPIO.output(23, GPIO.LOW)
-                        GPIO.output(24, GPIO.LOW)
+                        GPIO.output(12, GPIO.HIGH)
+                        GPIO.output(13, GPIO.LOW)
+                        GPIO.output(19, GPIO.LOW)
+                        GPIO.output(26, GPIO.LOW)
                         print "Rover Right"
         elif towhere == 'stop':
                         clean()
@@ -41,9 +41,9 @@ def carmove(towhere):
 if __name__ == '__main__':
         import sys
         towhere = sys.argv[1]
-        GPIO.setup(17, GPIO.OUT)
-        GPIO.setup(22, GPIO.OUT)
-        GPIO.setup(23, GPIO.OUT)
-        GPIO.setup(24, GPIO.OUT)
+        GPIO.setup(12, GPIO.OUT)
+        GPIO.setup(13, GPIO.OUT)
+        GPIO.setup(19, GPIO.OUT)
+        GPIO.setup(26, GPIO.OUT)
         carmove(towhere)
         #GPIO.cleanup()
